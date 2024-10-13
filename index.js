62e96e24c11f1184c2189c0def7f4a0ab3e1e562
AndreyKlimmov <andrey.klimmov@gmail.com>
2024-10-13 18:12:43 +0300

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 require('dotenv').config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const USER = process.env.USER_MONGO_DB;
 const PASSWORD = process.env.PASSWORD_MONGO_DB;
 console.log(USER);
@@ -13,10 +13,7 @@ console.log(USER);
 app.use(bodyParser.json());
 
 // MongoDB Connection
-mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@cluster0.psnbsei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0\'`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@cluster0.psnbsei.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0\'`);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
